43d7850b8baa899ab5f8f46fa9cdb51a6c844a5b
2011788 <2011788@brunelac.uk>
2021-10-26 13:42:43 +0100

    1

---
+++ main.py
@@ -0,0 +1,8 @@
+myList = ['knife', 'tentacle sweeper', 'lightsaber']
+weapon = input(print("Choose a weapon: 1: knife"
+                     "                 2: tentacle sweeper"
+                     "                 3: lightsaber"))
+if weapon == 'tentacle sweeper' and weapon in  myList:
+    print('You chose a tentacle sweeper.')
+else:
+    print('Wrong weapon choice')
